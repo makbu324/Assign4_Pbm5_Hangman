bae6481b1e203e65fb5282ab4a6bf10a26815632
Jonah <jonahm@bu.edu>
2023-10-13 15:52:26 -0400

    Two of the hint button functionalities have been added.

--- app/src/main/java/com/example/assign4_pbm5_hangman/MainActivity.kt
+++ app/src/main/java/com/example/assign4_pbm5_hangman/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.assign4_pbm5_hangman
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
@@ -8,9 +9,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import com.google.android.material.snackbar.Snackbar
 
-var wordNow: String = "______"
-var the_word: String = "RONALD"
-var hang_state: Int = 0
 
 interface OnDataPass {
     fun onDataPass(data: String)
@@ -19,16 +17,20 @@ interface OnDataPass {
 class MainActivity : AppCompatActivity(), OnDataPass {
     private lateinit var imgView: ImageView
     private lateinit var wordView: TextView
-    var coordinatorLayout: CoordinatorLayout? = null
+    private lateinit var hintButton: Button
+    private var hintCount: Int = 0
+    private var wordNow: String = "______"
+    private var the_word: String = ""
+    private var hang_state: Int = 0
+    private val Words = listOf("RONALD", "ACORNS", "BOXING", "BRONZE", "HIDDEN", "QUIRKY")
 
-    var layout: CoordinatorLayout? = null
 
     fun hangman_state_return(num: Int, alphabet: String): Int {
-        if (0 < num && num < 7)
-            Toast.makeText(this,
-                "Oops! \"" + alphabet + "\" is NOT in there...",
-                Toast.LENGTH_SHORT
-            ).show()
+        if (alphabet != "HintIncPic")
+            if (0 < num && num < 7)
+                Toast.makeText(this,
+                    "Oops! \"" + alphabet + "\" is NOT in there...",
+                    Toast.LENGTH_SHORT).show()
         if (num == 0) (return R.drawable.hangman_state_0)
         else if (num == 1) (return R.drawable.hangman_state_1)
         else if (num == 2) (return R.drawable.hangman_state_2)
@@ -78,9 +80,64 @@ class MainActivity : AppCompatActivity(), OnDataPass {
 
         imgView = findViewById(R.id.hangman_state)
         wordView = findViewById(R.id.whats_the_word)
+        hintButton = findViewById(R.id.hint_button)
 
+        chooseword()
+        wordNow = "______"
+        hang_state = 0
         wordView.text = wordNow
         imgView.setImageResource(hangman_state_return(hang_state, "#"))
+
+        hintButton.setOnClickListener{
+            hintCount++
+            HintTime()
+        }
     }
 
+    private fun chooseword(){
+        the_word = Words.random()
+    }
+    private fun HintTime() {
+        if (hang_state != 6) {
+            if (hintCount == 1) {
+                Toast.makeText(this, Hint4Word(the_word), Toast.LENGTH_LONG).show()
+            }
+            if (hintCount == 2) {
+                //Will disable half of the remaining letters & costs a turn
+                hang_state++
+                imgView.setImageResource(hangman_state_return(hang_state, "HintIncPic"))
+            }
+            if (hintCount == 3) {
+                //Will show all vowels & costs a turn will also disable all vowels
+                var foundVowels = false
+                for (i in the_word.indices) {
+                    val char = the_word[i]
+                    if (char in "AEIOU") {
+                        onDataPass(char.toString())
+                        foundVowels = true
+                    }
+                }
+                if (foundVowels) {
+                    hang_state++
+                    imgView.setImageResource(hangman_state_return(hang_state, "HintIncPic"))
+                } else {
+                    Toast.makeText(this, "No more vowels", Toast.LENGTH_SHORT).show()
+                }
+            }
+        } else {
+            Toast.makeText(this, "Hint not available", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun Hint4Word(word: String): String{
+        return when(word){
+            "RONALD" -> "Hint: A professor we all know"
+            "ACORNS" -> "Hint: Grows on trees"
+            "BOXING" -> "Hint: A sport"
+            "BRONZE" -> "Hint: A metal"
+            "HIDDEN" -> "Hint: Out of Sight"
+            "QUIRKY" -> "Hint: A little weird"
+            else -> "No hint available"
+        }
+    }
 }
